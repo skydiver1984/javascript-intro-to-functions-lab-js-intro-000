1c5480a052b55c96a02c2012996e0f60ee9b060f
Luis Hawkins <bladerunnerluis@gmail.com>
2020-03-08 02:11:20 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -11,3 +11,12 @@ console.log(string.toUpperCase())
 function logWhisper(string) {
   console.log(string.toLowerCase())
 }
+function sayHiToGrandma(string) {
+  if (string.toLowerCase() === string) {
+    return "I can't hear you!"
+  }
+  else if (string.toUpperCase() === string) {
+    return "YES INDEED!"
+  }
+    
+}
